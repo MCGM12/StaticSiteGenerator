5368aed8d856d1196aeee3ca5489c47353e5ed99
MCGM12 <matocraft506@gmail.com>
2025-06-10 19:56:16 -0700

    htmlnode and htmlnode tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,3 +10,15 @@ class HTMLNode():
 
     def to_html(self):
         raise NotImplementedError
+    
+    def props_to_html(self):
+        formatted = " ".join(f'{k}="{v}"' for k, v in self.props.items())
+        return (f'( {formatted} )')
+    
+    def __repr__(self):
+        # This is for printing the HTMLNode object to see all of its values.
+        print(f"HTMLNode: ")
+        print(f" - Tag: {self.tag}")
+        print(f" - Value: {self.value}")
+        print(f" - Children: {self.children}")
+        print(f" - Props: {self.props}")
