bf6493c03a946cca59034ca8787e1ec4ebfc5815
MCGM12 <matocraft506@gmail.com>
2025-06-14 20:55:11 -0700

    ye

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,4 +1,9 @@
 from htmlnode import HTMLNode
 
-class LeafNode():
-    
+class LeafNode(HTMLNode):
+    def __init__(self, value, **kwargs):
+        super().__init__(**kwargs)  # Do not pass children
+        self.value = value
+
+
+    def to_html(self):
