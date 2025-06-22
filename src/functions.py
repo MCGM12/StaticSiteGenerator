ec9adf6ec470bfe8343e8288a44ed08ca7e3dd0d
MCGM12 <matocraft506@gmail.com>
2025-06-22 13:30:54 -0700

    working on delimiter func

--- src/functions.py
+++ src/functions.py
@@ -30,3 +30,9 @@ def text_node_to_html_node(text_node):
             raise Exception("Image text node must have a URL")
         return_node = LeafNode("img", "", props={"src": text_node.url})
         return return_node
+
+
+def split_nodes_delimiter(old_nodes: TextNode, delimiter, text_type):
+    #old_nodes is the input, delimiter is the string character to split on, text_type is the type of text node to create
+    new_string = old_nodes.text.split(delimiter)
+    return new_string
